05c9161b5dc9c7032df454073dd6a88f20436282
Luke <46260371+AdmiralLuke@users.noreply.github.com>
2022-07-05 17:13:48 +0200

    Add files via upload

--- MergeSort.c
+++ MergeSort.c
@@ -2,11 +2,19 @@
 #include <stdlib.h>
 // #include <pthread.h>
 
+/**
+ * @brief Struct to pass the length from an array as an argument
+ * 
+ */
 typedef struct {
     int* a;
     int length;
 } IntArray;
 
+/**
+ * @brief struct for p_thread operations
+ * stores the splitted array parts and the thread_id
+ */
 typedef struct {
     int thread_id;
     IntArray array;
@@ -16,10 +24,21 @@ typedef struct {
     int size;
 } p_thread;
 
+/**
+ * @brief allocates int array with given size
+ * 
+ * @param size size of the allocated array
+ * @return int* allocated array of given size
+ */
 int* allocIntArray(int size) {
     return (int *)malloc(sizeof(int) * size);
 }
 
+/**
+ * @brief testing method to print the content of an array
+ * 
+ * @param a array (as struct IntArray for the length)
+ */
 void printArray(IntArray a) {
     int n = a.length;
     for (int i = 0; i < n; i++) {
@@ -28,6 +47,14 @@ void printArray(IntArray a) {
     printf("\n");
 }
 
+/**
+ * @brief copies the content of an array to a new array 
+ * 
+ * @param a input array
+ * @param from position in array where to start
+ * @param to position in array where to stop
+ * @return IntArray array with copied content from input array
+ */
 IntArray copyOfRange(IntArray a, int from, int to) {
     int n = to - from;
     // printf("%d %d %d\n", from, to, n);
@@ -83,7 +110,12 @@ int isSorted(IntArray a) {
     return 1;
 }
 
-/*
+/**
+ * @brief runs normal MergeSort when given 1 Thread
+ * splits array in n-thread parts
+ * @param a input array
+ * @param threadCount number of threads
+ */
 void parallelMerge(IntArray* a ,int threadCount) {
     if (threadCount < 2) {
         mergeSort(a);
@@ -92,24 +124,39 @@ void parallelMerge(IntArray* a ,int threadCount) {
         int tmpThreadStart = 0;
         for (int i = 0; i < threadCount; i++) {
             if (i == 0) {
-                p_thread[i] = {
+                threads[i] = {
                     .start = 0,
-                    .end = (int)(a->size / thread_count),
+                    .end = (int)(a->size / threadCouunt),
                     .size = .end - .start,
                     .array = copyOfRange(a, 0, .end),
                 };
-                tmpThreadStart = (int)(a->size / threadCount);
+                tmpThreadStart = (int)(a->length / threadCount);
             } else if (i != threadCount - 2) {
-                p_thread[i] = {
-                    .end = 
-                }
+                threads[i] = {
+                    .start = tmpThreadStart + 1,
+                    .end = .start + (int)(a->size / thread_count),
+                    .size = .end - .start,
+                    .a = copyOfRange(a, .start, .end),
+                };
+                tmpThreadStart += 1 + (int)(a->length / threadCount);
+            } else {
+                threads[i] = {
+                    .start = tmpThreadStart + 1;
+                    .end = .start + (int)(a->size / thread_count) + (a->size%thread_count),
+                    .size = .start - .end,
+                    .a = copyOfRange(a, start, end), 
+                };
             }
         }
     }
 }
 
-*/
-
+/**
+ * @brief generates array with random numbers in range of 0 to given size
+ * 
+ * @param size size of array and range of numbers
+ * @return int* random array with size-elements
+ */
 int* generateRandomArray(int size) {
     int* randArray = allocIntArray(size);
     for (int i = 0; i < size; i++) {
